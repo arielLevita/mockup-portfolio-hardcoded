6446857d972703b2456de609450f9ed31e269bf4
arielLevita <levita.ariel@gmail.com>
2023-04-15 00:36:49 -0300

    Link Amazing arreglado.

---
+++ data/ariel.js
@@ -0,0 +1,244 @@
+const ariel = [{
+    "nombre": "Ariel",
+    "apellido": "Levita",
+    "dni": 30182108,
+    "fechaNac": "1983-03-15",
+    "direccion": "Almafuerte 1644, Esquel, Argentina",
+    "telefono": "+5492945603365",
+    "correoElectronico": "levita.ariel@gmail.com",
+    "profesion": "Bedel, Docente, Desarrollador Web Full Stack",
+    "acercaDeMi": "Soy un docente, introduciéndome en el mundo del desarrollo web. En este momento, estoy enfocado en el aprendizaje de lenguajes para el desarrollo full-stack, y en la construcción de pequeñas aplicaciones web.",
+    "urlImagen": "https://i.imgur.com/LMfgEvQ.png",
+    "experienciaLaboral": [
+        {
+            id: 1,
+            "empresa": "Ministerio de Educación del Gobierno de la Ciudad Autónoma de Buenos Aires - Jardines Maternales y Jardines de Infantes",
+            "cargo": "Maestro de Sección",
+            "fechaInicio": "2009-05",
+            "continua": false,
+            "fechaFin": "2012-02",
+            "descripcion": "Mi primera experiencia como profesional de la educación fue esencial para fortalecer los aprendizajes construidos en el Profesorado. El trabajo amable y respetuoso con las familias me ayudó a deconstruir algunos estereotipos vinculados al rol masculino respecto del cuidado de niñas y niños pequeños, a la vez que me permitió implementar proyectos que hicieran foco sobre los aspectos educativos de la crianza de las y los más pequeños."
+        },
+        {
+            id: 2,
+            "empresa": "Ministerio de Educación de la Provincia de Chubut - Jardines Maternales y Jardines de Infantes Provinciales",
+            "cargo": "Maestro de Nivel Inicial",
+            "fechaInicio": "2012-02",
+            "continua": false,
+            "fechaFin": "2019-02",
+            "descripcion": "Comenzar a trabajar en una región en donde el cuerpo docente no fue formado por especialistas en el nivel, implicó conciliar diferencias conceptuales respecto de las prácticas educativas. En lo particular, además del trabajo propio de la docencia, mi trabajo estuvo más centrado en ampliar el abanico de acción, realizando propuestas áulicas e institucionales desde diferentes enfoques conceptuales."
+        },
+        {
+            id: 3,
+            "empresa": "Espacio Recreativo 'La Jungla'",
+            "cargo": "Docente de Talleres de Eduación no-formal",
+            "fechaInicio": "2019-03",
+            "continua": false,
+            "fechaFin": "2020-03",
+            "descripcion": "El Espacio de Juego fue una propuesta realizada en conjunto con una docente, dueña de un salón de eventos. Dentro de esta propuesta innovadora para la localidad, mi trabajo consistió en organizar e implementar las actividades recreativas, así como los talleres que el espacio ofrecía. El proyecto quedó trunco debido a la situación de aislamiento preventivo durante la pandemia de 2020-2021."
+        },
+        {
+            id: 4,
+            "empresa": "Universidad Nacional de la Patagonia san Juan Bosco - Facultad de Humanidades y Ciencias Sociales",
+            "cargo": "Ayudante de 2da en cátedra de Psicología",
+            "fechaInicio": "2020-03",
+            "continua": false,
+            "fechaFin": "2023-03",
+            "descripcion": "El rol del ayudante de 2da está directamente dirigido a la formación de las y los estudiantes en docencia universitaria. En este sentido, mi tarea estuvo orientada la confección de guías y material de lectura para las y los estudiantes, participación en las clases, participación en la corrección de trabajos prácticos, y apoyo a estudiantes antes sus consultas."
+        },
+        {
+            id: 5,
+            "empresa": "Ministerio de Educación de la Provincia de Chubut - Instituto Superior de Formación Docente N°809",
+            "cargo": "Docente de Nivel Superior",
+            "fechaInicio": "2015-04",
+            "continua": false,
+            "fechaFin": "2021-06",
+            "descripcion": "Dentro del Profesorado de Educación Inicial, estuve a cargo del dictado de dos unidades curriculares: el taller de “Producción de Recursos Didácticos para el Nivel Inicial” en donde, a diferencia del enfoque principalmente teórico que se utilizaba hasta el momento, se comenzó a utilizar una metodología de teoría-producción-socialización-retroalimentación, que permitió a las estudiantes formar parte del proceso educativo de sus compañeras, enriqueciendo la apropiación de los conocimientos; y la asignatura de “Sujetos de la Educación Inicial” en donde se pudieron incorporar diversos aspectos históricos y sociales, al abordaje fundamentalmente psicologicista que se realizaba hasta el momento."
+        },
+        {
+            id: 6,
+            "empresa": "Ministerio de Educación de la Provincia de Chubut - Instituto Superior de Formación Docente N°809",
+            "cargo": "Bedel en Oficina de Estudiantes",
+            "fechaInicio": "2021-06",
+            "continua": true,
+            "fechaFin": "continúa",
+            "descripcion": "Si bien la Bedelía es el área responsable de gestionar administrativamente las trayectorias formativas de los estudiantes, promover acciones y acompañar estas trayectorias para fortalecer los procesos de ingreso, permanencia y egreso, cuando las y los bedeles ganamos los concursos para acceder al cargo, presentamos diferentes líneas de acción particulares que permiten fortalecer y mejorar las tareas de la oficina. En mi caso particular, las propuestas se centran en un aspecto educativo, que mejora el acceso de la y los estudiantes a información relevante para la toma de decisiones útiles en sus trayectorias, por un lado, y a la construcción de un sistema que permita digitalizar parte de la información de las trayectorias, facilitando el acceso por parte de los actores institucionales y aumentando la transparencia ante la necesidad de consultas."
+        }
+    ],
+    "estudios": [
+        {
+            "formal": [
+                {
+                    id: 1,
+                    "titulo": "Bachiller Técnico Orientado en Gestión Administrativa",
+                    "institucion": "Escuela Técnica ORT",
+                    "anioInicio": "1995",
+                    "anioFin": "2000"
+                },
+                {
+                    id: 2,
+                    "titulo": "Profesor en Educación Inicial",
+                    "institucion": "Inst. Sup. de Profesorado en Educación Inicial Sara C. de Eccleston",
+                    "anioInicio": "2005",
+                    "anioFin": "2007"
+                },
+                {
+                    id: 3,
+                    "titulo": "Prof. y Lic. en Ciencias de la Educación",
+                    "institucion": "Universidad Nacional de la Patagonia San Juan Bosco",
+                    "anioInicio": "2015",
+                    "anioFin": "continúa"
+                }
+            ],
+            "noFormal": [
+                {
+                    id: 1,
+                    "titulo": "Bootcamp Desarrollo Web Front End",
+                    "organizador": "Argentina Programa + MindHub - Ministerio de Economía del Desarrollo",
+                    "campo": "Desarrollo Web",
+                    "fechaInicio": "2023-02",
+                    "fechaFin": "2023-04"
+                },
+                {
+                    id: 2,
+                    "titulo": "Desarrollo web Full-Stack (JavaScript + Node.js)",
+                    "organizador": "Codo a Codo - Agencia de Aprendizaje a lo Largo de la Vida - Ministerio de Educación de la Ciudad Autónoma de Buenos Aires",
+                    "campo": "Desarrollo Web",
+                    "fechaInicio": "2022-08",
+                    "fechaFin": "2022-12"
+                },
+                {
+                    id: 3,
+                    "titulo": "#YoProgramo",
+                    "organizador": "Argentina Programa - Ministerio de Desarrollo Productivo de la Nación",
+                    "campo": "Desarrollo Web",
+                    "fechaInicio": "2022-07",
+                    "fechaFin": "2023-04"
+                },
+                {
+                    id: 4,
+                    "titulo": "Nivel 8 de Inglés Técnico para Desarrolladores de Software (B1.2 MCER)",
+                    "organizador": "Centro Universitario de Idiomas - Universidad de Buenos Aires",
+                    "campo": "Desarrollo Web",
+                    "fechaInicio": "2022-12",
+                    "fechaFin": "2023-03"
+                },
+                {
+                    id: 5,
+                    "titulo": "Aulas Virtuales para el Nivel Superior",
+                    "organizador": "Instituto Nacional de Formación Docente - Ministerio de Educación de la Nación",
+                    "campo": "Docencia",
+                    "fechaInicio": "2022-09",
+                    "fechaFin": "2022-10"
+                },
+                {
+                    id: 6,
+                    "titulo": "Hacia una pedagogía del cuidado. El abordaje en la escuela de las situaciones relativas al suicidio",
+                    "organizador": "Instituto Nacional de Formación Docente - Ministerio de Educación de la Nación",
+                    "campo": "Docencia",
+                    "fechaInicio": "2022-08",
+                    "fechaFin": "2022-09"
+                },
+                {
+                    id: 7,
+                    "titulo": "Investigación y escritura: características y estrategias para la comunicación del conocimiento",
+                    "organizador": "Instituto Nacional de Formación Docente - Ministerio de Educación de la Nación",
+                    "campo": "Universidad",
+                    "fechaInicio": "2022-12",
+                    "fechaFin": "2022-11"
+                },
+                {
+                    id: 8,
+                    "titulo": "Ayudas Tecnológicas para el Estudio",
+                    "organizador": "Dirección de Educación a Distancia - Universidad Nacional de la Patagonia San Juan Bosco (UNPSJB)",
+                    "campo": "Universidad",
+                    "fechaInicio": "2021-05",
+                    "fechaFin": "2021-07"
+                }
+            ]
+        }
+    ],
+    "habilidades": [
+        {
+            "habilidadesDuras": [
+                {
+                    id: 1,
+                    "habilidad": "HTML",
+                    "puntajeSobreCinco": 2
+                },
+                {
+                    id: 2,
+                    "habilidad": "CSS",
+                    "puntajeSobreCinco": 2
+                },
+                {
+                    id: 3,
+                    "habilidad": "JavaScript",
+                    "puntajeSobreCinco": 2
+                },
+                {
+                    id: 4,
+                    "habilidad": "Bootstrap",
+                    "puntajeSobreCinco": 3
+                },
+                {
+                    id: 5,
+                    "habilidad": "Angular",
+                    "puntajeSobreCinco": 1
+                },
+                {
+                    id: 6,
+                    "habilidad": "Java",
+                    "puntajeSobreCinco": 1
+                },
+                {
+                    id: 7,
+                    "habilidad": "Spring Boot",
+                    "puntajeSobreCinco": 1
+                },
+                {
+                    id: 8,
+                    "habilidad": "Node.js",
+                    "puntajeSobreCinco": 1
+                },
+                {
+                    id: 9,
+                    "habilidad": "MySQL",
+                    "puntajeSobreCinco": 1
+                },
+                {
+                    id: 10,
+                    "habilidad": "Git / GitHub",
+                    "puntajeSobreCinco": 1
+                },
+            ],
+            "habilidadesBlandas": [
+                {
+                    id: 1,
+                    "habilidad": "Trabajo en equipo",
+                    "puntajeSobreCinco": 5
+                },
+                {
+                    id: 2,
+                    "habilidad": "Comprensión escrita",
+                    "puntajeSobreCinco": 5
+                },
+                {
+                    id: 3,
+                    "habilidad": "Expresión escrita",
+                    "puntajeSobreCinco": 5
+                },
+                {
+                    id: 4,
+                    "habilidad": "Seguridad en sí mismo",
+                    "puntajeSobreCinco": 5
+                },
+                {
+                    id: 5,
+                    "habilidad": "Estrategias para el aprendizaje",
+                    "puntajeSobreCinco": 5
+                }
+            ]
+        }
+    ]
+}]
